8b895ce2be33af7fde83a640e2b98ef5636a1d54
songStar0904 <1043328710@qq.com>
2021-07-28 23:45:45 +0800

    demo1

--- src/pages/Demo1/index.tsx
+++ src/pages/Demo1/index.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Demo1: React.FC = () => {
-  return <>demo1</>;
+  const [count, setCount] = useState(0);
+  console.log(count);
+  const alertCount = () => {
+    setTimeout(() => {
+      // alert 只能获取到点击按钮时的哪个状态
+      alert(count);
+    }, 3000);
+  };
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={alertCount}>alert</button>
+    </>
+  );
 };
 
 export default Demo1;
